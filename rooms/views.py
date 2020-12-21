70ac5afc4a2783eed1be0f75f5121550fc0bf190
Jongwan Kim <seulbiclosers@gmail.com>
2020-12-21 14:21:33 +0900

    #11.6

---
+++ rooms/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
+from . import models
+
+
+def all_rooms(request):
+    page = request.GET.get("page", 1)
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10, orphans=5)
+    try:
+        rooms = paginator.page(int(page))
+        return render(request, "rooms/home.html", {"page": rooms})
+    except EmptyPage:
+        return redirect("/") #redirect to 'Home(initial page)'. Used return here to prevent user's abnomral activity
+
