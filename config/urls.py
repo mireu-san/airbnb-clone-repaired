009d86ae320551c3f69b35f09cae5befef749b4a
mireu-san <seulbiclosers@gmail.com>
2021-01-12 17:35:27 +0900

    test

--- config/urls.py
+++ config/urls.py
@@ -3,8 +3,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path("", include("core.urls", namespace="core")),
+    path("rooms/", include("rooms.urls", namespace="rooms")),
+    path("users/", include("users.urls", namespace="users")),
     path("admin/", admin.site.urls),
 ]
 
