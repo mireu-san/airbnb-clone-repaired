19589df0ea0091f6cd2aef78a378d83caefb3150
azurchief <67046863+azurchief@users.noreply.github.com>
2021-01-15 23:27:58 +0900

    repair is in progress...

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.views.generic import FormView
 from django.urls import reverse_lazy
 from django.shortcuts import render, reverse
 from django.contrib.auth import authenticate, login, logout
-from . import forms
+from . import forms, models
 
 class LoginView(FormView):
     template_name = "users/login.html"
@@ -29,3 +29,24 @@ class SignUpView(FormView):
     success_url = reverse_lazy("core:home")
     initial = {"first_name": "Naruhodo", "last_name": "Ryuichi", "email": "seulbiclosers@gmail.com"}
 
+    def form_valid(self, form):
+        form.save()
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        user.verify_email()
+        return super().form_valid(form)
+
+def complete_verification(request, key):
+    try:
+        user = models.User.objects.get(email_secret=key)
+        user.email_verified = True
+        user.email_secret = ""
+        user.save()
+        # to do: add success message
+    except models.User.DoesNotExist:
+        # to do: add error message
+        pass
+    return redirect(reverse("core:home"))
