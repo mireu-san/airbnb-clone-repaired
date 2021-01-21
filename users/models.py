00bcda8bb4af3f0f383fd0207e06b31ee06183b4
azurchief <67046863+azurchief@users.noreply.github.com>
2021-01-21 20:17:16 +0900

    secure version - before tailwindcss

--- users/models.py
+++ users/models.py
@@ -30,6 +30,14 @@ class User(AbstractUser):
 
     CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
 
+    LOGIN_EMAIL = "email"
+    LOGIN_GITHUB = "github"
+    LOGIN_KAKAO = "kakao"
+
+    LOGIN_CHOICES = (
+        (LOGIN_EMAIL, "Email"), (LOGIN_GITHUB, "Github"), (LOGIN_KAKAO, "Kakao"),
+    )
+
     avatar = models.ImageField(upload_to="avatars", blank=True)
     gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
     bio = models.TextField(blank=True)
@@ -43,10 +51,11 @@ class User(AbstractUser):
     superhost = models.BooleanField(default=False)
     email_verified = models.BooleanField(default=False)
     email_secret = models.CharField(max_length=20, default="", blank=True)
+    login_method = models.CharField(max_length=50, choices=LOGIN_CHOICES, default=LOGIN_EMAIL)
 
     def verify_email(self):
         if self.email_verified is False:
-            secret = uuid.uuid4().hex[:20]
+            secret = "12345"
             self.email_secret = secret
             html_message = render_to_string(
                 "emails/verify_email.html", {"secret":secret}
